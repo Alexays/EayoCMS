3be77accb9ff841cd1d2b7fa6707ab872144ff9d
Leigende <alexisr245@gmail.com>
2017-07-02 17:39:53 +0200

    Update Apps.

--- apps/frontend/themes/default/assets/js/main.js
+++ apps/frontend/themes/default/assets/js/main.js
@@ -1,79 +1,84 @@
 /*jslint browser: true plusplus: true */
-/*global jQuery, Waves, alert*/
-jQuery(function($) {
+/*global jQuery*/
+window.Tether = {};
+jQuery(function ($) {
     'use strict';
-    var images = [];
-    var photos = [];
-    var eayo = window.eayo || {};
-    $(document).ready(function() {
-        eayo.item_grid();
-    });
-    $(window).on('load', function() {
+    const images = [
+        "http://arouillard.fr/data/uploads/wolf3d.jpg",
+        "http://arouillard.fr/data/uploads/raytracer.jpg",
+        "http://arouillard.fr/data/uploads/wireframe.jpg",
+        "http://arouillard.fr/data/uploads/AI.jpg",
+        "http://arouillard.fr/data/uploads/CMS.jpg"
+    ];
+    let photos = [];
+    let last_height = 0;
+    let width_scrollbar = 0;
+    let eayo = window.eayo || {};
+    $(document).ready(() => {
         eayo.init();
     });
-    $(window).on('resize', function() {
-        eayo.mobile_friendly();
+    $(window).on('resize', () => {
+        eayo.item_grid();
     });
-    /* Init Client system */
-    eayo.init = function() {
-    };
-    eayo.item_grid = function() {
-        var isotopeImagesReveal = function() {
-            // hide by default
-            $(".widget").hide();
-            $(".widget").imagesLoaded().progress(function(imgLoad, image) {
-                $(image.img).parents($(".widget")).delay(10).fadeIn();
-            });
 
-            return this;
-        };
-        var colWidth = function() {
-            var w = $('.grid').width(),
-                columnNum = 1,
-                columnWidth = 0;
-            if (w > 1200) {
-                columnNum = 3;
-            } else if (w > 900) {
-                columnNum = 3;
-            } else if (w > 600) {
-                columnNum = 3;
-            } else if (w > 300) {
-                columnNum = 2;
-            }
-            if ($('.grid .widget').length < columnNum) {
-                columnWidth = Math.floor(w / $('.grid .widget').length);
-            } else {
-                columnWidth = Math.floor(w / columnNum);
-            }
-            $('.grid').find('.widget').each(function() {
-                var $item = $(this),
-                    width = columnWidth,
-                    height = columnWidth;
-                $item.css({
-                    width: width
-                });
-            });
-            return columnWidth;
-        };
-        var isotope = function() {
-            $('.grid').isotope({
-                // set itemSelector so .grid-sizer is not used in layout
-                itemSelector: '.widget',
-                masonry: {
-                    columnWidth: colWidth()
+    /* Init Client system */
+    eayo.init = function () {
+        let up = 0;
+        //Get scrollbar width
+        document.getElementById("wrapper").style.overflowY = "scroll";
+        const tmp_scroll = document.getElementById('grid').clientWidth;
+        document.getElementById("wrapper").style.overflowY = null;
+        width_scrollbar = document.getElementById('grid').clientWidth - tmp_scroll;
+        //Add Images
+        let imageElements = [];
+        for (let i = 0; i < images.length; i++) {
+            imageElements[i] = new Image();
+            imageElements[i].setAttribute("class", "image" + i);
+            //lazy-load img.
+            imageElements[i].onload = function () {
+                photos.push({ src: this.src, ar: this.width / this.height });
+                if (++up === images.length) {
+                    document.getElementById("spinner").setAttribute("style", "display:none !important");
+                    eayo.item_grid();
                 }
-            });
-        };
-        isotope()
-        isotopeImagesReveal();
-        $(window).on('debouncedresize', isotope);
-    };
-    eayo.mobile_friendly = function() {
-        var width = $(window).width();
-        if (width < 768 && isMobileWindow === false) {
-            isMobileWindow = true;
-        } else if (isMobileWindow === true) {
-            isMobileWindow = false;
+            }
+            imageElements[i].src = images[i];
         }
+    };
+
+    /* Grid */
+    eayo.item_grid = function () {
+        let grid = document.getElementById('grid');
+        grid.style.width = "101%";
+        grid.innerHTML = "";
+        const ideal_height = parseInt(grid.clientHeight / photos.length);
+        const summed_width = photos.reduce((sum, p) => sum += p.ar * ideal_height, 0);
+        let viewport_width = grid.clientWidth;
+        if (document.getElementById('container').clientHeight < last_height)
+            viewport_width -= width_scrollbar;
+        const rows = Math.ceil(summed_width / viewport_width);
+        const weights = photos.map((p) => parseFloat(p.ar * 100));
+        const partition = part(weights, rows);
+        const x = photos.slice(0);
+        let index = 0;
+        let row_buffer = [];
+        for (let i = 0; i < partition.length; i++) {
+            let summed_ratios;
+            row_buffer = [];
+            for (let j = 0; j < partition[i].length; j++)
+                row_buffer.push(photos[index++])
+            summed_ratios = row_buffer.reduce((sum, p) => sum += p.ar, 0);
+            for (let k = 0; k < row_buffer.length; k++) {
+                const photo = row_buffer[k]
+                let elem = document.createElement("div");
+                elem.style.backgroundImage = "url('" + x.shift().src + "')";
+                elem.style.width = parseInt(viewport_width / summed_ratios * photo.ar) + "px";
+                elem.style.height = parseInt(viewport_width / summed_ratios) + "px";
+                elem.setAttribute("class", "photo");
+                grid.appendChild(elem)
+            };
+        }
+        last_height = document.getElementById('container').scrollHeight;
+        grid.style.width = "103%";
     };
 });
