b5a846e3e4e96cde6180689e6cc4ffe2ac4fd1eb
Leigende <alexisr245@gmail.com>
2017-07-14 13:01:02 +0200

    Change username.

--- apps/backend/controller/indexCtrl.php
+++ apps/backend/controller/indexCtrl.php
@@ -3,7 +3,7 @@
   * This file is part of EayoCMS.
   *
   * @package EayoCMS
-  * @author Alexis Rouillard / Leigende <contact@arouillard.fr>
+  * @author Alexis Rouillard / Alexays <contact@arouillard.fr>
   * @link http://arouillard.fr
   *
   * For the full copyright and license information, please view the LICENSE
